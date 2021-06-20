75de9a631455dd8acd24e7a6e33949bbb79014ba
William Xu <william-xu@live.com>
2021-06-20 11:41:21 +1000

    refactor

--- src/views/feed/index.tsx
+++ src/views/feed/index.tsx
@@ -24,17 +24,17 @@ const ArticleFeed = (list, saved, setSaved) => {
   return (
     <>
       {list.map((list) => (
-          <Article
+        <Article
           url={list.web_url}
-          multimedia={                  list.multimedia[0]
+          multimedia={list.multimedia[0]
             ? process.env.REACT_APP_IMAGE + "/" + list.multimedia[0].url
             : "https://static01.nyt.com/vi-assets/images/share/1200x675_nameplate.png"}
           headline={list.headline.main}
           by={list.byline.original}
-          date={moment( list.pub_date).format("MMMM DD YYYY | h:mm A")}
+          date={moment(list.pub_date).format("MMMM DD YYYY | h:mm A")}
           abstract={shrink(list.abstract)}
           onSave={onSave}
-          />
+        />
       ))}
     </>
   );
